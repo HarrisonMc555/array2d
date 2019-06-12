5cdbb9107e65cced106754589bf3f4c85d9786df
Harrison McCullough <mccullough.harrison@gmail.com>
2019-06-11 18:19:52 -0600

    Add tic-tac-toe to examples

---
+++ examples/tic-tac-toe/src/main.rs
@@ -0,0 +1,16 @@
+use array2d::Array2D;
+
+fn format_board(board: &Array2D<String>) -> String {
+    board
+        .rows_iter()
+        .map(|row_iter| row_iter.cloned().collect::<Vec<_>>().join("|"))
+        .collect::<Vec<_>>()
+        .join("\n-----\n")
+}
+
+fn main() {
+    let mut board = Array2D::fill_with(" ".to_string(), 3, 3);
+    println!("{}\n", format_board(&board));
+    board[(0, 2)] = "X".to_string();
+    println!("{}\n", format_board(&board));
+}
