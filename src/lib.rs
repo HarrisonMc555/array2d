52414dbf9746ebe2bc59d1d97cccf48c32094666
Harrison McCullough <mccullough.harrison@gmail.com>
2019-06-17 11:59:16 -0600

    Auto-derive Clone for Array2D

--- src/lib.rs
+++ src/lib.rs
@@ -148,7 +148,7 @@ use std::ops::{Index, IndexMut};
 use serde::{Serialize, Deserialize};
 
 /// A fixed sized two-dimensional array.
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct Array2D<T: Clone> {
     array: Vec<T>,
